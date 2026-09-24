fb4bf2fd88acdd2dea2c74e1ed9b77b13628aad9
Laeticia <75666423+laeticia-h@users.noreply.github.com>
2026-09-24 10:43:33 +0200

    Suppresion d'un ligne exo3

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -49,7 +49,6 @@ public class ListeSimpleTest {
         listeATester.ajout(1);
         listeATester.ajout(2);
         listeATester.ajout(3);
-        //System.out.println(listeATester);
        assertEquals("ListeSimple(Noeud(3), Noeud(2), Noeud(1))", listeATester.toString());
     }
 
